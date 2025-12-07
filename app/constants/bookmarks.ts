b2dc2a3928d5e14732e43e0ae6ae4c0e87b4e175
Emre <emrekurtt1655@gmail.com>
2025-12-07 15:44:46 +0100

    feat: add new bookmark for books with localization support

--- app/constants/bookmarks.ts
+++ app/constants/bookmarks.ts
@@ -8,5 +8,15 @@ export const bookmarks = [
     bgColor: "bg-green-200",
     circleColor: "bg-green-500",
     url: "https://app.raindrop.io/my/63917209",
+  },
+  {
+    id: "books",
+    titleEn: "Books",
+    titleDe: "Bücher",
+    subtitleEn: "Books I've read or want to read",
+    subtitleDe: "Bücher, die ich gelesen habe oder lesen möchte",
+    bgColor: "bg-orange-200",
+    circleColor: "bg-orange-500",
+    url: "https://app.raindrop.io/my/64031614",
   }
 ];
